228792d05f8098a94717a7d6bc934c1430c7b3e8
Karthik <karthik.kaliyaperumal@innoart.in>
2022-03-04 09:26:22 +0530

    change Ip

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://172.17.0.2:27017/CrudDB', (err)=>{
+mongoose.connect('mongodb://172.17.0.3:27017/CrudDB', (err)=>{
     if(!err)
         console.log('MongoDB Connection succeeded...');
     else
